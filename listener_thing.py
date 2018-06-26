fdd19d44ad4d41b9a3c0fcc6cde148b75ebef017
Isaac Watts <theicean@gmail.com>
2018-06-26 09:49:37 -0600

    listner notes and sample psuedo code

--- listener_thing.py
+++ listener_thing.py
@@ -1,8 +1,25 @@
 # listener_thing.py
 
-# avoid case
-# break apart and remove filler words
-
-
 # Link for mySQL db connector:
 # https://dev.mysql.com/doc/connector-python/en/connector-python-installation-binary.html
+
+# Basic commands as functions, acceptable param listeners
+
+# For Reference: https://github.com/JakeCooper/Zork/blob/master/Zork.py
+
+# exitCmd = re.compile('(?:Exit|Quit|Close|End|Suicide)', flags=re.IGNORECASE)
+# examineCmd = re.compile('(?:Look at|Examine|Inspect) (\w+)', flags=re.IGNORECASE)
+# moveCmd = re.compile('(?:Move|Go|Travel) (\w+)', flags=re.IGNORECASE)
+# inventoryCmd = re.compile('(?:Inventory|Items)', flags=re.IGNORECASE)
+# helpCmd = re.compile('(?:Help|H|\?|-H|-Help)(.*)', flags=re.IGNORECASE)
+
+
+# if examineCmd.match(cmd):
+# 		#Will not match with "examine". Need "examine what?" response, fix regex.
+# 		cmdParams = examineCmd.match(cmd).groups()
+# 		if cmdParams[0] == " ":
+# 			print("Examine what?")
+# 		elif cmdParams[0].lower() == 'room':
+# 			hero.room.examine()
+# 		else:
+# 			print("There is no {0} around here".format(cmdParams[0])) if not cmdParams[0].endswith("s") else print("There are no {0} around here".format(cmdParams[0])) #Crappy plural checker statement
