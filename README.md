2710de43c0957bffdbcab445051e6db96dcbf6cc
Isaac Watts <theicean@gmail.com>
2018-06-27 09:56:10 -0600

    added db file, set up validation function to see if user is in db

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # twitter-bot
 
 - requires tweepy, install in terminal with `pip install tweepy`
+- install SQLite for DB testing: `http://www.sqlitetutorial.net/download-install-sqlite/`,`https://www.sqlite.org/download.html`
--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 
 import tweepy
+import sqlite3
 from credentials import *
 
 #create an OAuthHandler instance
@@ -11,9 +12,22 @@ auth.set_access_token(access_token, access_token_secret)
 #Construct the API instance
 api = tweepy.API(auth) # create an API object
 
+conn = sqlite3.connect('test.db') # Connect to DB
+c = conn.cursor()
+
 #public_tweets = api.home_timeline()
 #for tweet in public_tweets:
 #    print(tweet.text)
+
+def user_validate(username):
+    c.execute("SELECT count(*) FROM components WHERE name = ?", (username))
+    data=c.fetchone()[0]
+    if data==0:
+        print('There is no user named %s'%name)
+        return false
+    else:
+        print('user %s found in %s row(s)'%(name,data))
+        return true
 
 def responder_agb(username, status_id, received_msg):
     print("Responding")
@@ -24,6 +38,13 @@ def responder_agb(username, status_id, received_msg):
     #	msg = game_listener(received_msg)
     #else:
     #	msg = admin_listener()
+    is_Mem = user_validate(username)
+
+    if(!is_Mem):
+        # check if they tweeted register, if true, add to DB
+        msg = "You are not yet signed up! Tweet \"Register\" to join!"
+    else:
+        msg = "You are already signed up!"
     
     api.update_status(msg, status_id)
 
